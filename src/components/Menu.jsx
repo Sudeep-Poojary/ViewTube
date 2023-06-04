0bf6177d9733e5ad4dde99f6967a5ab3b632f13d
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-06-04 18:50:11 +0530

    chore: Added Title in Menu Section and L/D Modes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -20,9 +20,9 @@ import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightne
 
 const Container = styled.div`
   flex: 1;
-  background-color: #202020;
+  background-color: ${({ theme }) => theme.bg};
   height: 100vh;
-  color: white;
+  color: ${({ theme }) => theme.text};
   font-style: 14px;
   position: sticky;
   top: 0;
@@ -54,7 +54,7 @@ const Item = styled.div`
 
 const Hr = styled.hr`
   margin: 15px 0px;
-  border: 0.5px solid #373737;
+  border: 0.5px solid ${({ theme }) => theme.soft};
 `;
 
 const Login = styled.div``;
@@ -73,7 +73,14 @@ const Button = styled.button`
   gap: 5px;
 `;
 
-const Menu = () => {
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 20px;
+`;
+
+const Menu = ({ darkMode, setDarkMode }) => {
   return (
     <Container>
       <Wrapper>
@@ -120,6 +127,8 @@ const Menu = () => {
         </Login>
 
         <Hr />
+
+        <Title>BEST OF VIEWTUBE</Title>
 
         <Item>
           <LibraryMusicOutlinedIcon />
@@ -168,7 +177,7 @@ const Menu = () => {
           Help
         </Item>
 
-        <Item>
+        <Item onClick={() => setDarkMode(!darkMode)}>
           <SettingsBrightnessOutlinedIcon />
           Light Mode
         </Item>
