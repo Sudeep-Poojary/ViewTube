01736483d640615f5fdceff9757a0383b395e00e
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-06-11 21:59:57 +0530

    chore: Added the content and recommendation part

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,7 +1,110 @@
 import React from "react";
+import styled from "styled-components";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import PlaylistAddOutlinedIcon from "@mui/icons-material/PlaylistAddOutlined";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+
+const VideoWrapper = styled.div``;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Recommendation = styled.div`
+  flex: 2;
+`;
 
 const Video = () => {
-  return <div>Video</div>;
+  return (
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/8q8tfH-40SI"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+
+        <Title>Test Video</Title>
+
+        <Details>
+          <Info>7,000,000 views ● Jun 10, 2023</Info>
+
+          <Buttons>
+            <Button>
+              <ThumbUpOutlinedIcon />
+              500
+            </Button>
+
+            <Button>
+              <ThumbDownOffAltOutlinedIcon />
+              Dislike
+            </Button>
+
+            <Button>
+              <ReplyOutlinedIcon />
+              Share
+            </Button>
+
+            <Button>
+              <PlaylistAddOutlinedIcon />
+              Save
+            </Button>
+          </Buttons>
+        </Details>
+
+        <Hr />
+      </Content>
+      <Recommendation>Recommendation</Recommendation>
+    </Container>
+  );
 };
 
 export default Video;
